17235f7f565693fcc0946e9cbccc305dff72deb7
Julius Ramirez <48529138+NerdRez@users.noreply.github.com>
2023-04-09 21:59:48 -0700

    Implemented insert() & Comments
    Finished insert (should test further) and also added a majority of the comments.

--- AVLTree.java
+++ AVLTree.java
@@ -1,5 +1,19 @@
+/**
+ * FILE: AVLTree.java
+ * CLASS: CSc 345 - Spring 2023
+ * PURPOSE: The purpose of this file is to hold code which can create an AVL
+ * Tree. An AVL Tree
+ * is a self balancing binary search tree (SBBST).
+ */
 public class AVLTree {
+    /**
+     * This represents how many nodes are currently in the tree
+     */
     private int size;
+    /**
+     * The root node. The root may switch to another node in order to
+     * maintain balance within the tree.
+     */
     private Node root;
 
     public AVLTree() {
@@ -39,7 +53,7 @@ public class AVLTree {
         } else if (toDelete.getLeft() != null && toDelete.getRight() != null) {
             // contains left/right sub-trees
             // TODO probably should implement checker so less work is done (ex: choose when
-            // TODO to go left/right)
+            // to go left/right)
             Node beforeNext = toDelete.getRight();
             Node next = toDelete.getRight().getLeft();
             while (next != null && next.getLeft() != null) {
@@ -55,112 +69,314 @@ public class AVLTree {
         // TODO update height and then balance tree
     }
 
+    /**
+     * This function prints out the tree with indentation in order to see the
+     * structure of the tree.
+     */
     public void printTree() {
-        stringHelper(0, root);
+        System.out.println("--------------------");
+        printHelper(0, root, "RT:");
+        System.out.println("--------------------");
+
     }
 
-    private void stringHelper(int depth, Node node) {
-
+    /**
+     * This helper function works recursively in order to print out
+     * the structure of the tree. It does a pre-order traversal and
+     * prints out identifiers in order to see the structure more easily.
+     * 
+     * @param depth    an int representing how deep the node is from the root
+     * @param node     the node to be printed
+     * @param identity a String (either RT,R, or L) representing the position of
+     *                 this node relative to the parent
+     */
+    private void printHelper(int depth, Node node, String identity) {
         String indent = "";
         for (int i = 0; i < depth; i++) {
-            indent += '\t';
+            indent += "  ";
         }
         if (node == null) {
             System.out.println(indent + "[]");
             return;
         }
-        System.out.println(indent + node);
-        stringHelper(depth + 1, node.getLeft());
-        stringHelper(depth + 1, node.getRight());
+        System.out.println(indent + identity + node);
+        printHelper(depth + 1, node.getRight(), "R");
+        printHelper(depth + 1, node.getLeft(), "L");
+
     }
 
     public void insert(int value) {
-        if (root == null) {
-            root = new Node(value);
-        }
-        root = insertHelper(value);
+        root = insertHelper(root, value);
         this.size++;
     }
 
-    private Node insertHelper(int value) {
-        return null;
+    /**
+     * This helper function inserts the node into the tree, returning
+     * Node objects in order to maintain balance after inserting the value.
+     * 
+     * @param node  the current Node object, or null
+     * @param value the value to be inserted
+     * @return a Node object which represents the new root of the subtree (or
+     *         possibly the entire tree)
+     */
+    private Node insertHelper(Node node, int value) {
+        if (node == null) {
+            return new Node(value);
+        }
+        if (value < node.getVal()) {
+            node.setRight(insertHelper(node.getRight(), value));
+        } else {
+            node.setLeft(insertHelper(node.getLeft(), value));
+        }
+        node.updateHeight();
+        return balanceTree(node);
     }
 
+    /**
+     * This function is called whenever a node becomes inserted so that
+     * the balance factor is maintained.
+     * 
+     * @param node the root of the tree that is to be balanced
+     * @return the root of the newly balanced tree
+     */
+    private Node balanceTree(Node node) {
+        int balance = node.getBalanceFactor();
+        // If a node becomes unbalanced, we must balance it and return the newly
+        // balanced tree
+        if (balance == -2) {
+            /*
+             * This code is responsible for balancing left heavy trees. The cases
+             * signal the direction of the imbalance from the current node
+             * (Ex: left-right means node has a left subtree which has a right subtree)
+             */
+            if (node.getLeft().getBalanceFactor() <= 0) {
+                // left-left case: / ==> ^
+                return rotateRight(node);
+            } else {
+                // left-right case: < ==> / ==> ^
+                node.setLeft(rotateLeft(node.getLeft())); // This transforms the tree to be left-left
+                return rotateRight(node);
+            }
+        } else if (balance == 2) {
+            /*
+             * This code is responsible for balancing right heavy trees. The cases
+             * signal the direction of the imbalance from the current node
+             * (Ex: right-left means node has a right subtree which has a left subtree)
+             */
+            if (node.getRight().getBalanceFactor() >= 0) {
+                // right-right case: \ ==> ^
+                return rotateLeft(node);
+            } else {
+                // right-left case: > ==> \ ==> ^
+                node.setRight(rotateRight(node.getRight())); // This transforms the tree to be right-right
+                return rotateLeft(node);
+            }
+        }
+        return node; // if nothing has to be balanced, we can return the node as is
+    }
+
+    /**
+     * This helper function rotates the tree right, or in other words
+     * shifts the nodes so that the left subtree becomes the new root
+     * 
+     * @param node the node that will be shifted
+     * @return the new root of the tree
+     */
+    private Node rotateRight(Node node) {
+        Node newRoot = node.getLeft();
+        node.setLeft(newRoot.getRight());
+        newRoot.setRight(node);
+        node.updateHeight();
+        newRoot.updateHeight();
+        return newRoot;
+    }
+
+    /**
+     * This helper function rotates the tree left, or in other words
+     * shifts the nodes so that the right subtree becomes the new root
+     * 
+     * @param node the node that will be shifted
+     * @return the new root of the tree
+     */
+    private Node rotateLeft(Node node) {
+        Node newRoot = node.getRight();
+        node.setRight(newRoot.getLeft());
+        newRoot.setLeft(node);
+        node.updateHeight();
+        newRoot.updateHeight();
+        return newRoot;
+    }
+
+    /**
+     * Returns a boolean indicating if the value exists within the tree or not.
+     * 
+     * @param value the integer to be found
+     * @return true if it exists in the tree, false if not
+     */
     public boolean search(int value) {
-        if (root == null)
-            return false;
         Node currentNode = root;
-        int currentVal = currentNode.getVal();
-        while (currentVal != value && currentNode != null) {
-            // only greater/lesser comparison due to equal condition for while loop
-            if (currentVal < value) {
+        while (currentNode != null) {
+            int currentVal = currentNode.getVal();
+            if (currentVal == value) {
+                return true;
+            } else if (currentVal < value) {
                 currentNode = currentNode.getRight();
             } else {
                 currentNode = currentNode.getLeft();
             }
-            if (currentNode != null) { // TODO can we simplify?
-                currentVal = currentNode.getVal();
-            }
         }
-        return currentVal == value;
+        return false;
     }
 
+    /**
+     * Returns the amount of Nodes within the tree
+     * 
+     * @return
+     */
     public int size() {
         return this.size;
     }
 
     public static void main(String[] args) {
         System.out.println("TESTING");
+        AVLTree tree = new AVLTree();
+        tree.insert(10);
+        tree.insert(6);
+        tree.printTree();
+        tree.insert(3);
+        tree.printTree();
+        tree.insert(9);
+        tree.insert(7);
+        tree.printTree();
+        tree.insert(15);
+        tree.printTree();
+        tree.insert(13);
+        tree.printTree();
+
     }
 }
 
 class Node {
-    private Node left;
-    private Node right;
+    /**
+     * The left subtree which contains values lesser than the current node's value
+     */
+    private Node left = null;
+    /**
+     * The right subtree which contains values greater than the current node's value
+     */
+    private Node right = null;
+    /**
+     * The value held by the node. It is assumed that no duplicate values will
+     * be within the tree or inserted into the tree.
+     */
     private int val;
-    private int height;
+    /**
+     * This represents the furthest distance from a leaf node.
+     * Leaf nodes have a height of zero.
+     */
+    private int height = 0;
+    /**
+     * The balance factor of a particular node is calculated by subtracting
+     * the height of the left subtree from the right subtree.
+     * 
+     * Once a tree has been balanced, it will always be equal to either: {-1,0,1}
+     */
+    private int balanceFactor = 0;
 
+    /**
+     * The default constroctor for Node. Sets the value of the node to the given
+     * value.
+     * 
+     * @param val any integer
+     */
     public Node(int val) {
-        this.left = null;
-        this.right = null;
+        this.val = val;
     }
 
+    /**
+     * Sets the nodes left subtree to the given node
+     * 
+     * @param n a Node object or null
+     */
     public void setLeft(Node n) {
         this.left = n;
     }
 
+    /**
+     * Sets the nodes right subtree to the given node
+     * 
+     * @param n a Node object or null
+     */
     public void setRight(Node n) {
         this.right = n;
     }
 
-    public void setHeight(int val) {
-        this.height = val;
-    }
-
+    /**
+     * Sets the val of Node to the given value
+     * 
+     * @param val any integer
+     */
     public void setVal(int val) {
         this.val = val;
     }
 
-    @Override
-    public String toString() {
-        // TODO
-        return "[Node: " + this.val + "]";
+    /**
+     * This function updates the height and balance factor values of the node.
+     * It is called in the insertHelper() method after the node has been inserted
+     * into the tree in order to determine if balancing the tree is necessary.
+     */
+    protected void updateHeight() {
+        int leftHeight = this.left != null ? this.left.height : -1;
+        int rightHeight = this.right != null ? this.right.height : -1;
+        int maxHeight = leftHeight < rightHeight ? rightHeight : leftHeight;
+        this.height = 1 + maxHeight;
+        this.balanceFactor = rightHeight - leftHeight;
     }
 
+    @Override
+    public String toString() {
+        return "[V,BF,H: " + this.val + ',' + this.balanceFactor + ',' + this.height + "]";
+    }
+
+    /**
+     * Returns the value of the node
+     * 
+     * @return an int which represents the nodes value
+     */
     public int getVal() {
         return this.val;
     }
 
+    /**
+     * Returns the balance factor of the node
+     * 
+     * @return the balance factor of the node
+     * @see balanceFactor
+     */
+    public int getBalanceFactor() {
+        return this.balanceFactor;
+    }
+
+    /**
+     * Returns the left subtree of the node
+     * 
+     * @return a Node object, or null
+     */
     public Node getLeft() {
         return this.left;
     }
 
+    /**
+     * Returns the right subtree of the node
+     * 
+     * @return a Node object, or null
+     */
     public Node getRight() {
         return this.right;
     }
 
     /**
-     * Returns the heigh of the node
+     * Returns the height of the node
      * 
      * @return the height of the node (-1,0,1)
      */
